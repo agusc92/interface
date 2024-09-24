ed037a94be768374fedd06a793d8b958650655c8
Agustin <agustinlabattaglia92@gmail.com>
2024-09-24 18:29:53 -0300

    realizado el estilo del paint

--- tp2_entregable/EscritorHtml.js
+++ tp2_entregable/EscritorHtml.js
@@ -14,7 +14,7 @@ class EscritorHtml{
     elemento.innerHTML = '<li><button value="monoCromo"><img src="./assets/mono-cromo.png"></button></li>';
     elemento.innerHTML += '<li><button value="negativo"><img src="./assets/negativo.png"></button></li>';
     elemento.innerHTML += '<li><button value="sepia"><img src="./assets/sepia.png"></button></li>';
-    elemento.innerHTML += '<li><button value="brillo"><img src="./assets/brillo.png"></button><input type="number" value=100 id="brillo"></li>';
+    elemento.innerHTML += '<li><button value="brillo"><img src="./assets/brillo.png"></button><input type="range" value=100 id="brillo" min="0" max="200"></li>';
     elemento.innerHTML += '<li><button value="blur"><img src="./assets/blur.png"></button></li>';
     elemento.innerHTML += '<li><button value="saturacion"><img src="./assets/saturacion.png"></button></li>';
     elemento.innerHTML += '<li><button value="detectBordes"><img src="./assets/borde.png"></button></li>';
